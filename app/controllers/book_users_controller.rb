8125af6e5b3800c76f5048141c6142271e138492
zestos <zestos@gmail.com>
2013-11-15 13:49:37 -0800

    Working add + delete from list

--- app/controllers/book_users_controller.rb
+++ app/controllers/book_users_controller.rb
@@ -2,15 +2,20 @@ class BookUsersController < ApplicationController
   before_filter :authorize
 
   def index
-    @books = current_user.books
+    @books = current_user.books.uniq { |book| book.title }
   end
 
   def create
-    BookUser.create(book_id: params[:book_id], user_id: params[:user_id])
+    if !BookUser.find_by_book_id(params[:book_id])
+      BookUser.create(book_id: params[:book_id], user_id: current_user.id)
+    end
+    render nothing: true
   end
 
   def destroy
-    BookUser.delete(book_id: params[:book_id])
+    id = BookUser.where(book_id: params[:id])
+    BookUser.delete(id)
+    render nothing: true
   end
 
 end
